c872dfb1fa8aee054b30b8dfc8221de3d4f6930b
Rodrigo Franco <rshina@hotmail.com>
2026-06-11 19:22:41 -0300

    first commit initial configurations

---
+++ src/main/java/com/rshinna/mybot_api/MybotApiApplication.java
@@ -0,0 +1,13 @@
+package com.rshinna.mybot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rshinna/mybot_api/MybotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rshinna.mybot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
